f957f24970cc033ad37f8b02bfd482e32a5777e6
Suhail Abdi <suhailabdi857@gmail.com>
2025-03-27 17:32:28 +0300

    hopefully this is it

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -22,7 +22,7 @@ export const clerkWebhook = async (req, res) => {
             case 'user.created':{
                 const userData ={
                     _id: data.id,
-                    email: data.email_addresses[0].email_addresses,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " +data.last_name,
                     image:data.image_url,
                     resume: ''
--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     resume:{type:String},
-    image: {type:String},
+    image: {type:String,required:true},
 
 })
 const User = mongoose.model("User", userSchema);
