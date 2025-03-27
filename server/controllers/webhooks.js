278429f7298fa1bcc2620bda42ed50a973c79aa6
Suhail Abdi <suhailabdi857@gmail.com>
2025-03-27 11:17:23 +0300

    fixed internal server error

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,5 @@
 import { Webhook } from "svix";
-import User from "../models/user";
+import User from "../models/user.js";
 //API controller function
 
 export const clerkWebhook = async (req, res) => {
